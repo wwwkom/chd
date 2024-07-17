199837630b8fb7c7da0c275f4ae39d1210c2acdf
wwwkom <40160603+wwwkom@users.noreply.github.com>
2024-07-17 16:48:16 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CHD'
+copyright = '2024, Sophia'
+author = 'Sophia'
 
 release = '0.1'
 version = '0.1.0'
